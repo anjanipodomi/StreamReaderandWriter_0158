f55ff036034a9090b259efba66dc8c14547b8c0b
anjanipodomi <anjanidihapsari@gmail.com>
2024-06-04 10:12:35 +0700

    membuat program inti dengan data dalam bentuk string, membuka file dalam mode menulis, dan menunjuk ke sebuah nama file.

--- StreamReaderandWriter/StreamReaderandWriter.cpp
+++ StreamReaderandWriter/StreamReaderandWriter.cpp
@@ -7,6 +7,19 @@ int main()
 {
 	//untuk menyimoan data berbentuk string
 	string baris;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true)
+	{
+
+	}
 }
 
 
