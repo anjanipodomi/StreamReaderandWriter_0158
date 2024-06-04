3795304463ed0fa442a8e6e6ccdb9ac8cb3c918d
anjanipodomi <anjanidihapsari@gmail.com>
2024-06-04 10:08:33 +0700

    Add project files.

---
+++ StreamReaderandWriter/StreamReaderandWriter.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//untuk menyimoan data berbentuk string
+	string baris;
+}
+
+
+
+
+
+
+
+
+int main()
+{
+  
+}
