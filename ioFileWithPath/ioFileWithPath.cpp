0eaac7f0c86e7553465b2ed2f04a97b43cc52978
anjanipodomi <anjanidihapsari@gmail.com>
2024-06-04 10:37:19 +0700

    membuka file dalam mode menulis dan /membuka ke sebuah nama file serta menampilakan ">= Menulis file, \'q\' untuk keluar"

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -12,4 +12,10 @@ int main()
 	cout << "Masukkan Nama File : ";
 	cin >> NamaFile;
    
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//membuka ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
